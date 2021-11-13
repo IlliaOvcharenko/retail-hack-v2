e2485e678796c6d3775ff15afebdf475d83aecd8
Illia Ovcharenko <iovcharenko@MacBook-Air-Illia.local>
2021-11-13 21:17:36 +0200

    parallel eval

--- scripts/create_solution.py
+++ scripts/create_solution.py
@@ -1,77 +1,76 @@
+import sys,os
+sys.path.append(os.getcwd())
+
 import pandas as pd
 import networkx as nx
 
 from tqdm import tqdm
 from itertools import combinations
-from create_dark_store_graph import create_darkstore_graph
+from fire import Fire
 
-cheques_df = pd.read_csv("data/cheques_public.csv", sep=";")
-darkstore_df = pd.read_csv("data/darkstore_map.csv", sep=";")
+from scripts.create_dark_store_graph import create_darkstore_graph
+from scripts.create_prods_graph import create_graph
 
-g = nx.Graph()
-
-for cheque_id, cheque_df in tqdm(cheques_df.groupby("CHEQUEID", sort=True)):
-    for _, cheque_item in cheque_df.iterrows():
-        if g.has_node(cheque_item.LAGERID):
-            g.nodes[cheque_item.LAGERID]["num_of_cheques"] += 1
-            g.nodes[cheque_item.LAGERID]["overall_amount"] += cheque_item.KOLVO
-            
-        else:
-            node_info = { 
-                "num_of_cheques": 1,
-                "overall_amount": cheque_item.KOLVO
-            }
-            g.add_node(cheque_item.LAGERID, **node_info)
-
-    produc_ids =  cheque_df.LAGERID.unique()
-    for prod_1, prod_2 in list(combinations(produc_ids, 2)):
-        if g.has_edge(prod_1, prod_2):
-            g.edges[prod_1, prod_2]['weight'] += 1
-        else:
-            g.add_edge(prod_1, prod_2, weight=1)
-
-popular = sorted(list(g.nodes.data()), key=lambda n: n[1]["overall_amount"], reverse=True)
-popular = [int(x[0]) for x in popular]
-
-print('10 most popular products:', popular[:10])
-
-for n in g.nodes:
-    g.nodes[n]["mean_amount"]  = g.nodes[n]["overall_amount"] / g.nodes[n]["num_of_cheques"]
-
-best_items = []
-
-n_popular = 6 # Тут різні варіанти пробувати
-
-for popular_item in popular:
-  if popular_item not in best_items:
-    best_items.append(popular_item)
-    closest_prods = sorted([(n, g.edges[popular_item, n]) for n in list(g.neighbors(popular_item))], key=lambda n: n[1]["weight"], reverse=True)
-    n_popular_list = [int(x[0]) for x in closest_prods[:n_popular]]
-    best_items += list(set(n_popular_list) - set(best_items))
-
-print()
-print('Likely to buy list:', best_items)
-
-G = create_darkstore_graph()
 
 def get_closest_lagers_to_start(G):
-    dist_to_zero = pd.DataFrame()
-    all_nodes = []
-    for i in range(1, 45):
-          list_of_lagers = [i+level for level in [0.1, 0.2, 0.3]]
-          all_nodes = all_nodes + list_of_lagers
+        dist_to_zero = pd.DataFrame()
+        all_nodes = []
+        for i in range(1, 45):
+              list_of_lagers = [i+level for level in [0.1, 0.2, 0.3]]
+              all_nodes = all_nodes + list_of_lagers
 
-    for node in all_nodes:
-        shortest_path_l = nx.shortest_path_length(G, 0, node, weight='weight')
-        dist_to_zero.loc[node, 'dist_to_zero'] = shortest_path_l
-    
-    dist_to_zero = dist_to_zero.reset_index()
-    dist_to_zero = dist_to_zero.sort_values('dist_to_zero')
-    dist_to_zero['SECTION'] = dist_to_zero['index'].apply(lambda x: int(str(x).split('.')[0]))
-    dist_to_zero['LEVEL'] = dist_to_zero['index'].apply(lambda x: int(str(x).split('.')[1]))
-    dist_to_zero = dist_to_zero.drop(['index', 'dist_to_zero'], axis=1)
-    return dist_to_zero.reset_index(drop=True)
+        for node in all_nodes:
+            shortest_path_l = nx.shortest_path_length(G, 0, node, weight='weight')
+            dist_to_zero.loc[node, 'dist_to_zero'] = shortest_path_l
 
-dist_to_zero = get_closest_lagers_to_start(G)
-dist_to_zero['LAGERID'] = best_items
-dist_to_zero.to_csv('data/darkstore_good.csv', index=False, sep=';')
+        dist_to_zero = dist_to_zero.reset_index()
+        dist_to_zero = dist_to_zero.sort_values('dist_to_zero')
+        dist_to_zero['SECTION'] = dist_to_zero['index'].apply(lambda x: int(str(x).split('.')[0]))
+        dist_to_zero['LEVEL'] = dist_to_zero['index'].apply(lambda x: int(str(x).split('.')[1]))
+        dist_to_zero = dist_to_zero.drop(['index', 'dist_to_zero'], axis=1)
+        return dist_to_zero.reset_index(drop=True)
+
+def main(
+    save_filename
+):
+    cheques_df = pd.read_csv("data/cheques_public.csv", sep=";")
+    darkstore_df = pd.read_csv("data/darkstore_map.csv", sep=";")
+
+    g = create_graph(cheques_df)
+    popular = sorted(list(g.nodes.data()), key=lambda n: n[1]["overall_amount"], reverse=True)
+    popular = [int(x[0]) for x in popular]
+
+    print('10 most popular products:', popular[:10])
+
+    for n in g.nodes:
+        g.nodes[n]["mean_amount"]  = g.nodes[n]["overall_amount"] / g.nodes[n]["num_of_cheques"]
+
+    best_items = []
+
+    n_popular = 6 # Тут різні варіанти пробувати
+
+    for popular_item in popular:
+      if popular_item not in best_items:
+        best_items.append(popular_item)
+        closest_prods = sorted(
+            [(n, g.edges[popular_item, n]) for n in list(g.neighbors(popular_item))],
+            key=lambda n: n[1]["weight"],
+            reverse=True
+        )
+        n_popular_list = [int(x[0]) for x in closest_prods[:n_popular]]
+        best_items += list(set(n_popular_list) - set(best_items))
+
+    print()
+    print('Likely to buy list:', best_items)
+
+    G = create_darkstore_graph()
+
+    dist_to_zero = get_closest_lagers_to_start(G)
+    dist_to_zero['LAGERID'] = best_items
+
+    dist_to_zero.to_csv(save_filename, index=False, sep=';')
+
+
+if __name__ == "__main__":
+    Fire(main)
+
--- scripts/evaluate_darkstore_tsp.py
+++ scripts/evaluate_darkstore_tsp.py
@@ -1,9 +1,16 @@
+import sys,os
+sys.path.append(os.getcwd())
+
 import pandas as pd
 import networkx as nx
+
 from tqdm import tqdm
-from create_dark_store_graph import create_darkstore_graph
-cheques_public = pd.read_csv('data/cheques_public.csv', sep=';')
-darkstore_map = pd.read_csv('data/darkstore_map.csv', sep=';')
+from fire import Fire
+from functools import partial
+from multiprocessing import Pool
+
+from scripts.create_dark_store_graph import create_darkstore_graph
+
 
 def evaluate_cheque_tsp(one_cheque, darkstore_map, G):
   multiple_items = []
@@ -17,7 +24,7 @@ def evaluate_cheque_tsp(one_cheque, darkstore_map, G):
           if quantity > 1:
             multiple_items.append((graph_node, int(quantity)))
           path.append(graph_node)
-  
+
   total_time = 0
   shortest_path = nx.approximation.traveling_salesman_problem(G, nodes=[0] + path + [0])
   for first, second in zip(shortest_path, shortest_path[1:]):
@@ -32,10 +39,36 @@ def evaluate_cheque_tsp(one_cheque, darkstore_map, G):
     total_time += time * quantity
   return total_time
 
-G = create_darkstore_graph()
 
-total_total_time = 0
-for cheque in tqdm(list(cheques_public.CHEQUEID.unique())):
-  total_total_time += evaluate_cheque_tsp(cheques_public[cheques_public.CHEQUEID == cheque], darkstore_map, G)
-print()
-print('Total time:', total_total_time, 'Mean total time:', total_total_time / len(cheques_public.CHEQUEID.unique()))
+def main(
+    darkstore_map_fn,
+    n_proc=8,
+):
+    cheques_public = pd.read_csv('data/cheques_public.csv', sep=';')
+    darkstore_map = pd.read_csv(darkstore_map_fn, sep=';')
+
+
+    G = create_darkstore_graph()
+
+    total_total_time = 0
+    cheques = [ch for _, ch in cheques_public.groupby("CHEQUEID")]
+
+    with Pool(n_proc) as p:
+        map_func = partial(
+            evaluate_cheque_tsp,
+            darkstore_map=darkstore_map,
+            G=G,
+        )
+
+        total_total_time = sum(list(tqdm(p.imap(map_func, cheques),
+                                         total=len(cheques),
+                                         desc="process cheques")))
+
+    print()
+    print('Total time:', total_total_time,
+          'Mean total time:', total_total_time / len(cheques_public.CHEQUEID.unique()))
+
+
+if __name__ == "__main__":
+    Fire(main)
+
